7d1b3d5fe0c4532aedd740eb825d9ea5268d452f
Divyansh Sharma <divyanshsharma4507@gmail.com>
2026-02-19 18:10:54 +0530

    fix: remove default marketplace value from optimize function parameters

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -3,7 +3,7 @@ import { optimizeByAsin } from '../services/product.service';
 
 export async function optimize(req: Request, res: Response, next: NextFunction) {
   try {
-    const { asin, marketplace = 'amazon.in' } = req.body;
+    const { asin, marketplace } = req.body;
     const result = await optimizeByAsin(asin.toUpperCase(), marketplace);
     res.status(201).json({ success: true, data: result });
   } catch (err) {
